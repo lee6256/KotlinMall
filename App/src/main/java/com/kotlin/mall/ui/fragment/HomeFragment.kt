572014763447c5699430bfd7c6a5a62e55b41071
LiZiQi <lzq.leeleg@qq.com>
2018-10-03 22:12:44 +0800

    Featured: Add topic. Refactored: Gradle dependencies

--- App/src/main/java/com/kotlin/mall/ui/fragment/HomeFragment.kt
+++ App/src/main/java/com/kotlin/mall/ui/fragment/HomeFragment.kt
@@ -1,19 +1,21 @@
 package com.kotlin.mall.ui.fragment
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kotlin.base.common.BaseConstant
 import com.kotlin.base.ui.fragment.BaseFragment
 import com.kotlin.base.widgets.BannerImageLoader
 import com.kotlin.mall.R
-import com.kotlin.mall.common.HOME_BANNER_FOUR
-import com.kotlin.mall.common.HOME_BANNER_ONE
-import com.kotlin.mall.common.HOME_BANNER_THREE
-import com.kotlin.mall.common.HOME_BANNER_TWO
+import com.kotlin.mall.common.*
+import com.kotlin.mall.ui.adapter.HomeDiscountAdapter
+import com.kotlin.mall.ui.adapter.TopicAdapter
 import com.youth.banner.BannerConfig
 import com.youth.banner.Transformer
 import kotlinx.android.synthetic.main.fragment_home.*
+import me.crosswall.lib.coverflow.CoverFlow
 
 class HomeFragment : BaseFragment() {
 
@@ -26,6 +28,8 @@ class HomeFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
         initBanner()
         initNews()
+        initDiscount()
+        initTopic()
     }
 
     private fun initBanner() {
@@ -39,5 +43,23 @@ class HomeFragment : BaseFragment() {
 
     private fun initNews() {
         mFlipperView.setData(arrayOf("123", "456"))
+    }
+
+    private fun initDiscount() {
+        val manager = LinearLayoutManager(context)
+        manager.orientation = LinearLayoutManager.HORIZONTAL
+        mHomeDiscountRv.layoutManager = manager
+
+        val discountAdapter = HomeDiscountAdapter(activity)
+        mHomeDiscountRv.adapter = discountAdapter
+        discountAdapter.setData(mutableListOf(HOME_DISCOUNT_ONE, HOME_DISCOUNT_TWO, HOME_DISCOUNT_THREE, HOME_DISCOUNT_FOUR, HOME_DISCOUNT_FIVE))
+    }
+
+    private fun initTopic() {
+        mTopicPager.adapter = TopicAdapter(context, listOf(HOME_TOPIC_ONE, HOME_TOPIC_TWO, HOME_TOPIC_THREE, HOME_TOPIC_FOUR, HOME_TOPIC_FIVE))
+        mTopicPager.currentItem = 1
+        mTopicPager.offscreenPageLimit = 5
+
+        CoverFlow.Builder().with(mTopicPager).scale(0.3f).pagerMargin(-30.0f).spaceSize(0.0f).build()
     }
 }
--- BaseLibrary/src/main/java/com/kotlin/base/ext/CommonExt.kt
+++ BaseLibrary/src/main/java/com/kotlin/base/ext/CommonExt.kt
@@ -3,10 +3,12 @@ package com.kotlin.base.ext
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import com.kotlin.base.data.protocol.BaseResp
 import com.kotlin.base.rx.BaseFunc
 import com.kotlin.base.rx.BaseFuncBoolean
 import com.kotlin.base.rx.BaseSubscriber
+import com.kotlin.base.utils.GlideUtils
 import com.kotlin.base.widgets.DefaultTextWatcher
 import com.trello.rxlifecycle.LifecycleProvider
 import rx.Observable
@@ -47,4 +49,8 @@ fun Button.enable(et: EditText, method: () -> Boolean) {
             btn.isEnabled = method()
         }
     })
+}
+
+fun ImageView.loadUrl(url: String) {
+    GlideUtils.loadUrlImage(context, url, this)
 }
