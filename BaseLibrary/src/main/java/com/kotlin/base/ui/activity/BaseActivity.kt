9dcfca6f5a7c4d7d80ead536cb208a20784deae7
LiZiQi <lzq.leeleg@qq.com>
2018-11-28 22:27:14 +0800

    Featured: Finished SKU PopWindow.

--- BaseLibrary/src/main/java/com/kotlin/base/ui/activity/BaseActivity.kt
+++ BaseLibrary/src/main/java/com/kotlin/base/ui/activity/BaseActivity.kt
@@ -1,13 +1,16 @@
 package com.kotlin.base.ui.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
 import com.kotlin.base.common.AppManager
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
+import org.jetbrains.anko.find
 
 /**
  * Created by HelloWorld on 2018/5/27.
  */
-open class BaseActivity: RxAppCompatActivity() {
+open class BaseActivity : RxAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,4 +21,10 @@ open class BaseActivity: RxAppCompatActivity() {
         super.onDestroy()
         AppManager.instance.finishActivity(this)
     }
+
+    val contentView: View
+        get() {
+            val content = find<FrameLayout>(android.R.id.content)
+            return content.getChildAt(0)
+        }
 }
