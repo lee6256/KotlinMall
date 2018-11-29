86f0f5ceae0673b00d6a7926aa44cd8d9a925d4b
LiZiQi <lzq.leeleg@qq.com>
2018-11-29 22:45:17 +0800

    Featured: Integration ARouter.

--- GoodsCenter/src/main/java/com/kotlin/goods/ui/activity/GoodsDetailActivity.kt
+++ GoodsCenter/src/main/java/com/kotlin/goods/ui/activity/GoodsDetailActivity.kt
@@ -2,9 +2,12 @@ package com.kotlin.goods.ui.activity
 
 import android.os.Bundle
 import android.support.design.widget.TabLayout
+import com.alibaba.android.arouter.launcher.ARouter
+import com.kotlin.base.ext.onClick
 import com.kotlin.base.ui.activity.BaseActivity
 import com.kotlin.goods.R
 import com.kotlin.goods.ui.adapter.GoodsDetailVpAdapter
+import com.kotlin.provider.router.RouterPath
 import kotlinx.android.synthetic.main.activity_goods_detail.*
 
 class GoodsDetailActivity : BaseActivity() {
@@ -20,5 +23,9 @@ class GoodsDetailActivity : BaseActivity() {
 
         mGoodsDetailVp.adapter = GoodsDetailVpAdapter(supportFragmentManager, this)
         mGoodsDetailTab.setupWithViewPager(mGoodsDetailVp)
+
+        mAddCartBtn.onClick {
+            ARouter.getInstance().build(RouterPath.UserCenter.PATH_LOGIN).navigation()
+        }
     }
 }
