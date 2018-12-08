886c62c6b19ebf96da282c567fe05a74699345b8
LiZiQi <lzq.leeleg@qq.com>
2018-12-08 20:52:21 +0800

    Featured: Add shopping cart service interface and realize it.

---
+++ GoodsCenter/src/main/java/com/kotlin/goods/service/impl/CartServiceImpl.kt
@@ -0,0 +1,30 @@
+package com.kotlin.goods.service.impl
+
+import com.kotlin.base.ext.convert
+import com.kotlin.goods.data.protocol.CartGoods
+import com.kotlin.goods.data.repository.CartRepository
+import com.kotlin.goods.data.repository.CategoryRepository
+import com.kotlin.goods.service.CartService
+import rx.Observable
+import javax.inject.Inject
+
+class CartServiceImpl @Inject constructor(): CartService {
+    @Inject
+    lateinit var repository: CartRepository
+
+    override fun getCartList(): Observable<MutableList<CartGoods>?> {
+        return repository.getCartList().convert()
+    }
+
+    override fun addCart(goodsId: Int, goodsDesc: String, goodsIcon: String, goodsPrice: Long, goodsCount: Int, goodsSku: String): Observable<Int> {
+        return repository.addCart(goodsId, goodsDesc, goodsIcon, goodsPrice, goodsCount, goodsSku).convert()
+    }
+
+    override fun deleteCartList(cartIdList: List<Int>): Observable<String> {
+        return repository.deleteCartList(cartIdList).convert()
+    }
+
+    override fun submitCart(goodsList: List<CartGoods>, totalPrice: Long): Observable<Int> {
+        return repository.submitCart(goodsList, totalPrice).convert()
+    }
+}
