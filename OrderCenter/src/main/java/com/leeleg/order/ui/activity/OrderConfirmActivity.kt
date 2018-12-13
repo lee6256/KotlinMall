981c06f4d50a9a5334c2626bc5e30d7f568fff1a
LiZiQi <lzq.leeleg@qq.com>
2018-12-13 23:20:08 +0800

    Feature: Finished order confirm presenter.

--- OrderCenter/src/main/java/com/leeleg/order/ui/activity/OrderConfirmActivity.kt
+++ OrderCenter/src/main/java/com/leeleg/order/ui/activity/OrderConfirmActivity.kt
@@ -2,15 +2,41 @@ package com.leeleg.order.ui.activity
 
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.kotlin.base.ui.activity.BaseActivity
+import com.kotlin.base.ui.activity.BaseMvpActivity
+import com.kotlin.base.utils.YuanFenConverter
+import com.kotlin.provider.common.ProviderConstant
 import com.kotlin.provider.router.RouterPath
 import com.leeleg.order.R
+import com.leeleg.order.data.protocol.Order
+import com.leeleg.order.injection.component.DaggerOrderComponent
+import com.leeleg.order.injection.module.OrderModule
+import com.leeleg.order.presenter.OrderConfirmPresenter
+import com.leeleg.order.presenter.view.OrderConfirmView
+import kotlinx.android.synthetic.main.activity_order_confirm.*
 
 @Route(path = RouterPath.OrderCenter.PATH_ORDER_CONFIRM)
-class OrderConfirmActivity : BaseActivity() {
+class OrderConfirmActivity : BaseMvpActivity<OrderConfirmPresenter>(), OrderConfirmView {
+
+    private var mOrderId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_confirm)
+
+        mOrderId = intent.getIntExtra(ProviderConstant.KEY_ORDER_ID, -1)
+        mPresenter.getOrderById(mOrderId)
+    }
+
+    override fun injectComponent() {
+        DaggerOrderComponent.builder()
+                .activityComponent(activityComponent)
+                .orderModule(OrderModule())
+                .build()
+                .inject(this)
+        mPresenter.mView = this
+    }
+
+    override fun onGetOrderByIdResult(result: Order) {
+        mTotalPriceTv.text = "合计：${YuanFenConverter.changeF2YWithUnit(result.totalPrice)}"
     }
 }
