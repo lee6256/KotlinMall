f60f557c2baf3837bf534b43944cd61d91b5c36a
LiZiQi <lzq.leeleg@qq.com>
2018-12-20 15:35:15 +0800

    Featured: Finished "OrderGoodsAdapter".

--- OrderCenter/src/main/java/com/leeleg/order/ui/activity/OrderConfirmActivity.kt
+++ OrderCenter/src/main/java/com/leeleg/order/ui/activity/OrderConfirmActivity.kt
@@ -1,6 +1,8 @@
 package com.leeleg.order.ui.activity
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.kotlin.base.ui.activity.BaseMvpActivity
 import com.kotlin.base.utils.YuanFenConverter
@@ -12,18 +14,32 @@ import com.leeleg.order.injection.component.DaggerOrderComponent
 import com.leeleg.order.injection.module.OrderModule
 import com.leeleg.order.presenter.OrderConfirmPresenter
 import com.leeleg.order.presenter.view.OrderConfirmView
+import com.leeleg.order.ui.adapter.OrderGoodsAdapter
 import kotlinx.android.synthetic.main.activity_order_confirm.*
 
 @Route(path = RouterPath.OrderCenter.PATH_ORDER_CONFIRM)
 class OrderConfirmActivity : BaseMvpActivity<OrderConfirmPresenter>(), OrderConfirmView {
 
-    private var mOrderId: Int = 0
+    @Autowired(name = ProviderConstant.KEY_ORDER_ID)
+    @JvmField
+    var mOrderId: Int = 0
+    private lateinit var mAdapter: OrderGoodsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_confirm)
 
-        mOrderId = intent.getIntExtra(ProviderConstant.KEY_ORDER_ID, -1)
+        initView()
+        loadData()
+    }
+
+    private fun initView() {
+        mOrderGoodsRv.layoutManager = LinearLayoutManager(this)
+        mAdapter = OrderGoodsAdapter(this)
+        mOrderGoodsRv.adapter = mAdapter
+    }
+
+    private fun loadData() {
         mPresenter.getOrderById(mOrderId)
     }
 
@@ -37,6 +53,7 @@ class OrderConfirmActivity : BaseMvpActivity<OrderConfirmPresenter>(), OrderConf
     }
 
     override fun onGetOrderByIdResult(result: Order) {
+        mAdapter.setData(result.orderGoodsList)
         mTotalPriceTv.text = "合计：${YuanFenConverter.changeF2YWithUnit(result.totalPrice)}"
     }
 }
