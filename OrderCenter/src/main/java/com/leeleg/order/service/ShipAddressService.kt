90a30c70566139f0c01cbc6c5fccc56275b87785
LiZiQi <lzq.leeleg@qq.com>
2018-12-20 15:58:08 +0800

    Fixed: "ShipAddressService".

--- OrderCenter/src/main/java/com/leeleg/order/service/ShipAddressService.kt
+++ OrderCenter/src/main/java/com/leeleg/order/service/ShipAddressService.kt
@@ -4,7 +4,7 @@ import com.leeleg.order.data.protocol.ShipAddress
 import rx.Observable
 
 interface ShipAddressService {
-    fun addShipAddress(shipUserName: String, shipUserMobile: String, shipAddress: String): Observable<String>
+    fun addShipAddress(shipUserName: String, shipUserMobile: String, shipAddress: String): Observable<Boolean>
     fun deleteShipAddress(id: Int): Observable<String>
     fun editShipAddress(id: Int, shipUserName: String, shipUserMobile: String, shipAddress: String,
                         shipIsDefault: Int): Observable<String>
--- OrderCenter/src/main/java/com/leeleg/order/service/impl/ShipAddressServiceImpl.kt
+++ OrderCenter/src/main/java/com/leeleg/order/service/impl/ShipAddressServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.leeleg.order.service.impl
 
 import com.kotlin.base.ext.convert
+import com.kotlin.base.ext.convertBoolean
 import com.leeleg.order.data.protocol.ShipAddress
 import com.leeleg.order.data.repository.ShipAddressRepository
 import com.leeleg.order.service.ShipAddressService
@@ -12,8 +13,8 @@ class ShipAddressServiceImpl @Inject constructor(): ShipAddressService {
     @Inject
     lateinit var repository: ShipAddressRepository
 
-    override fun addShipAddress(shipUserName: String, shipUserMobile: String, shipAddress: String): Observable<String> {
-        return repository.addShipAddress(shipUserName, shipUserMobile, shipAddress).convert()
+    override fun addShipAddress(shipUserName: String, shipUserMobile: String, shipAddress: String): Observable<Boolean> {
+        return repository.addShipAddress(shipUserName, shipUserMobile, shipAddress).convertBoolean()
     }
 
     override fun deleteShipAddress(id: Int): Observable<String> {
