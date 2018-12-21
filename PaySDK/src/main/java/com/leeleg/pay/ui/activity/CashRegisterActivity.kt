37393d5a2e3857fa38d19ce6e5b975076ee8e47b
LiZiQi <lzq.leeleg@qq.com>
2018-12-21 15:08:17 +0800

    Featured: Finished get pay sign business.

--- PaySDK/src/main/java/com/leeleg/pay/ui/activity/CashRegisterActivity.kt
+++ PaySDK/src/main/java/com/leeleg/pay/ui/activity/CashRegisterActivity.kt
@@ -3,16 +3,30 @@ package com.leeleg.pay.ui.activity
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.alibaba.android.arouter.launcher.ARouter
-import com.kotlin.base.ui.activity.BaseActivity
+import com.kotlin.base.ext.onClick
+import com.kotlin.base.ui.activity.BaseMvpActivity
 import com.kotlin.base.utils.YuanFenConverter
 import com.kotlin.provider.common.ProviderConstant
 import com.kotlin.provider.router.RouterPath
 import com.leeleg.pay.R
+import com.leeleg.pay.injection.component.DaggerPayComponent
+import com.leeleg.pay.injection.module.PayModule
+import com.leeleg.pay.presenter.PayPresenter
+import com.leeleg.pay.presenter.view.PayView
 import kotlinx.android.synthetic.main.activity_cash_register.*
+import org.jetbrains.anko.toast
 
 @Route(path = RouterPath.PaySDK.PATH_PAY)
-class CashRegisterActivity : BaseActivity() {
+class CashRegisterActivity : BaseMvpActivity<PayPresenter>(), PayView {
+
+    override fun injectComponent() {
+        DaggerPayComponent.builder()
+                .activityComponent(activityComponent)
+                .payModule(PayModule())
+                .build()
+                .inject(this)
+        mPresenter.mView = this
+    }
 
     @Autowired(name = ProviderConstant.KEY_ORDER_ID)
     @JvmField
@@ -26,11 +40,18 @@ class CashRegisterActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cash_register)
 
-        ARouter.getInstance().inject(this)
         initView()
     }
 
     private fun initView() {
         mTotalPriceTv.text = YuanFenConverter.changeF2YWithUnit(mTotalPrice)
+
+        mPayBtn.onClick {
+            mPresenter.getPaySign(mOrderId, mTotalPrice)
+        }
+    }
+
+    override fun onGetPaySignResult(result: String) {
+        toast(result)
     }
 }
