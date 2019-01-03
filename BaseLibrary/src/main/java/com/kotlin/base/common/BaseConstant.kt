4a434a59660c506be80f81463818c0e8aac84bfe
qlzq <lzq.leeleg@qq.com>
2019-01-03 15:53:50 +0800

    Create BaseConstant.kt

---
+++ BaseLibrary/src/main/java/com/kotlin/base/common/BaseConstant.kt
@@ -0,0 +1,14 @@
+class BaseConstant {
+    companion object {
+        const val IMAGE_SERVER_ADDRESS = "Your owe image server address"
+        const val BUCKET_NAME = "Your owe bucket name"
+        const val ACCESS_KEY = "Your owe access key"
+        const val SECRET_KEY = "Your owe secret key"
+
+        const val SERVER_ADDRESS = "Your owe server address"
+
+        const val TABLE_PREFS = "Your owe pre file name"
+
+        const val KEY_SP_TOKEN = "Your owe token key"
+    }
+}
